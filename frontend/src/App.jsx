a1dd3ff8c499b99cfca1fbdf1174a35cc81c4649
nanthakumar-dev <nantha656565@gmail.com>
2025-03-15 08:06:56 -0700

    Up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return <h1>{message || "Loading..."}</h1>;
+}
+
+export default App;
